331e625b165cffb3abaf29156b4caadd13b1ee14
kang <hankangkang66@163.com>
2016-07-22 15:56:35 +0800

    cmd debug

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -16,9 +16,10 @@ class ConvertController extends Controller
             return self::$response->error(CODE_C_UPLOAD_FILE_TYPE_ERR);
         }
         $name = $file->getClientOriginalName();
-        $path = $file->getRealPath();
+        $file->move(APP_TEMPFILE_OUTPUT_PATH,$name);
 
-        $cmd = '/home/kang/Downloads/wkhtmltox/bin/wkhtmltopdf '.$path.' '.$name.'.pdf';
+
+        $cmd = '/home/kang/Downloads/wkhtmltox/bin/wkhtmltopdf '.APP_TEMPFILE_OUTPUT_PATH.'/'.$name.' '.APP_TEMPFILE_OUTPUT_PATH.'/'.$name.'.pdf';
         $out = shell_exec($cmd);
         return self::$response->success(['data'=>$out]);
     }
